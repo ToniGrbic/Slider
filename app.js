aeb92912d2d4959b3a6707ce3f2537eb805a3e9f
ToniGrbic <tonigrbic.5@gmail.com>
2022-09-07 23:41:21 +0200

    first commit

--- app.js
+++ app.js
@@ -1,57 +1,90 @@
-const imageSlider = $('.imageSlider')
-const sliderImages = $('.imageSlider img')
+const imageSliderTop = $('.topSlider')
+const imageSliderBottom = $('.bottomSlider')
+const sliderImagesTop = $('.topSlider img')
+const sliderImagesBottom = $('.bottomSlider img')
 const prevBtn = $('#prevBtn')
 const nextBtn = $('#nextBtn')
 
-let counter = sliderImages.length //counter prati koja slika je u fokusu, pomoću njega tu sliku selectamo
-let slideWidth = 0
+const topImgCount = sliderImagesTop.length
+const bottomImgCount = sliderImagesBottom.length
+
+//counter prati koja slika je u fokusu, pomoću njega tu sliku selectamo
+let counterTop = topImgCount
+let counterBottom = bottomImgCount 
 let direction
 
-const slide = (slideWidth)=>{
-    imageSlider.css('transform', `translateX(${slideWidth}px)`)
+const slide = (slideWidthTop, slideWidthBottom)=>{
+    imageSliderTop.css('transform', `translateX(${slideWidthTop}px)`)
+    imageSliderBottom.css('transform', `translateX(${slideWidthBottom}px)`)
 }
 
-const getImgWidth = (counter)=>{
-    let activeImgWidth = $(`#img${counter}`).outerWidth() //
-    return activeImgWidth
+const getTopImgWidth = (counter)=>{
+    return $(`#img-top-${counter}`).outerWidth() 
+}
+
+const getBottomImgWidth = (counter)=>{
+    return $(`#img-bottom-${counter}`).outerWidth() 
 }
 
 prevBtn.on('click',function(){
     
         direction = 'left'
-        let activeImgWidth = getImgWidth(counter)
-        if(counter === 1){
-            counter = sliderImages.length
+        let activeTopImgWidth = getTopImgWidth(counterTop)
+        let activeBottomImgWidth = getBottomImgWidth(counterBottom)
+
+        if(counterTop === 1 && counterBottom > 1 ){
+            counterTop = topImgCount
+            --counterBottom
+        }else if(counterBottom === 1 && counterTop > 1){
+           counterBottom = bottomImgCount
+           --counterTop
+        }else if(counterBottom === 1 && counterTop === 1){
+            counterTop = topImgCount
+            counterBottom = bottomImgCount
         }else{
-           --counter 
+            --counterTop
+            --counterBottom
         }
-        slideWidth = activeImgWidth
-        slide(slideWidth)
+        slide(activeTopImgWidth, activeBottomImgWidth)
 })
 
 nextBtn.on('click',function(){
-        direction = 'rigth'
-        if(counter === sliderImages.length){
-            counter = 1
+
+        direction = 'right'
+        
+        if(counterTop === topImgCount  && counterBottom < bottomImgCount){
+            counterTop = 1
+            ++counterBottom
+        }else if(counterBottom === bottomImgCount && counterTop < topImgCount){
+           counterBottom = 1
+           ++counterTop
+        }else if((counterTop === topImgCount) && (counterBottom === bottomImgCount)){
+            counterTop = 1
+            counterBottom = 1
         }else{
-            ++counter
+            ++counterTop
+            ++counterBottom 
         }
-        let nextImgWidth = getImgWidth(counter)
-        slideWidth = -(nextImgWidth)
-        slide(slideWidth)
+        let nextImgWidthTop = getTopImgWidth(counterTop)
+        let nextImgWidthBottom = getBottomImgWidth(counterBottom)
+        slide(-nextImgWidthTop, -nextImgWidthBottom)
 })
 
-imageSlider.on('transitionend', function(){
-    if(direction === 'left')
-        imageSlider.prepend($('.imageSlider img:last-child'))
-    else if(direction === 'rigth') 
-        imageSlider.append($('.imageSlider img:first-child'))
+imageSliderBottom.on('transitionend', addImages)
 
-    imageSlider.css('transition', 'none')     
-    imageSlider.css('transform', `translateX(0px)`)
-    setTimeout(function(){
-        imageSlider.css('transition', 'all 0.2s') 
-    })
-})
-
+function addImages(){
+    if(direction === 'left'){
+        imageSliderTop.prepend($('.topSlider img:last-child'))
+        imageSliderBottom.prepend($('.bottomSlider img:last-child'))
+    }   
+    else if(direction === 'right') {
+        imageSliderTop.append($(`.topSlider img:first-child` ))
+        imageSliderBottom.append($('.bottomSlider img:first-child'))
+    }
+    $('.imageSlider div').css('transition', 'none')     
+    $('.imageSlider div').css('transform', `translateX(0px)`)
+    setTimeout(()=>{
+        $('.imageSlider div').css('transition', 'all 0.5s') 
+    }) 
+}
 
